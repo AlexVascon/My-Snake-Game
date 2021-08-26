ae012f1be2244067d8f62f9e32b7c3a423c1a059
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2021-08-26 16:14:50 +0200

    fixed dead snake

--- js/canvasSnake.js
+++ js/canvasSnake.js
@@ -137,7 +137,7 @@ class CanvasSnake {
             (function(i, context) {
                 setTimeout(function() {
                     ctx.fillRect(context.body[i].x, context.body[i].y, 100,100 )
-                }, 2000 / context.body.length)
+                }, 100 * (context.body.length - i))
             })(i, this)
         }
     }
